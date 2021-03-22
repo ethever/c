cc846b2520f0799956e047aab77ed51ba865cf77
famouscat8 <3142362556@qq.com>
2021-03-22 19:03:18 +0800

    update:添加了一些注释

--- TCP/client.c
+++ TCP/client.c
@@ -15,6 +15,80 @@ int main(){
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	serv_addr.sin_port = htons(1234);
+	// int connect(int sockfd, struct sockaddr *serv_addr, int addrlen);
+	// sockfd: 通过socket函数产生的文件描述符
+	// serv_addr:
+	// 	sockaddr_in: 要连接的IP4地址和端口
+	// 	sockaddr_in6: 要连接的IPv6地址和端口
+	// addrlen: serv_addr长度
+	//
+	// 阻塞模式下connect的返回值和参数设置:
+	// 阻塞和非阻塞不是connect函数的属性
+	// 而是通过socket函数获取的文件描述符
+	// 所指向的抽象文件的属性 所以需要通过操作socket
+	// 获取文件描述符fd(file description)来对其设置为
+	// 非阻塞 默认情况下socket文件是阻塞的
+	//
+	// 建立阻塞的socket文件描述符时
+	// 调用connect函数即完成了整个过程
+	// 连接成功返回0 连接失败返回-1
+	// 返回-1时表明该连接发生了错误 即没有
+	// 成功建立连接 并会将发生的错误记录在
+	// 一个全局变量errno中 errno的取值有...
+	//
+	// 注意: inet_pton 和 socket 函数都有可能调用失败 
+	// 需要对其返回值处理
+	//
+	// 非阻塞模式下connect的返回值和参数设置
+	// 对于非阻塞的connect连接，其不会在调用connect时进行阻塞
+	// 而是立即返回 如果返回0 说明调用connect的时候会没来得及
+	// 让connect返回便已经建立了连接 如果返回-1 errno会记录一个
+	// 错误码 如果该错误码为EINPROGRESS 则说明该连接还正在建立
+	// 并不能说明该连接出错 一个非阻塞connect的样例:
+	//
+	// int flag;
+	// int fd = socket(AF_INET, SOCK_STREAM, 0);
+	// 获取文件描述符属性
+	// flag = fcntl(connect_fd, F_GETFLK, 0);
+	// 将文件描述符设置为非阻塞
+	// fcntl(connect_fd, F_SETFLK, flag | 0_NONBLOCK);
+	// struct sockaddr_in addr = {0};
+	// addr.sin_family = AF_INET;
+	// addr.sin_port = htons(80);
+	// inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+	// for(;;){
+	//	flag = connect(fd, &addr, len);
+	//	if(flag == 0){
+	//		printf("connect success!\n");
+	//		break;
+	//	} else {
+	//		if(errno != EINPROGRESS){
+	//			printf("connect error!" "errno: %d \n ", errno);
+	//			break;
+	//		} else {
+	//			printf("connection is being established \n")
+	//			sleep(1);
+	//			continue;
+	//		}
+	//	}
+	//
+	// }
+	// 
+	//
+	//
+	// fcntl 函数:
+	// int fcntl(int fd, int cmd, ...);
+	// 常见用法
+	// 1. 复制一个文件描述符
+	// 2. 获取或设置文件状态标志
+	// 3. 获得、设置文件记录锁
+	//
+	// 获得、设置记录锁 (cmd = F_GETLK, F_SETLK, F_SETLKW)
+	// F_GETLK: 获得上锁的状态
+	// F_SETLK: 非阻塞上锁
+	// F_SETLKW: 阻塞上锁
+	//
+	//
 	connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
 	// 读取服务器传回的数据
--- TCP/main.c
+++ TCP/main.c
@@ -19,23 +19,88 @@ int main(){
 	int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
 	// 将套接字和IP、端口绑定
+	// sockaddr_in: 
+	// 	sa_family_t sin_family: 地址族(address family)
+	// 	uint16_t sin_port: 16位TCP/UDP端口号
+	// 	struct in_addr sin_addr: 32位Ip地址
+	// 		In_addr_t s_addr: 32位Ipv4地址
+	// 	char sin_zero[8]: 不使用
 	struct sockaddr_in serv_addr;
 	// 每个字节都用0填充
 	memset(&serv_addr, 0, sizeof(serv_addr));
 	// 使用IPv4地址
 	serv_addr.sin_family = AF_INET;
 	// 具体的Ip地址
+	// uint32 inet_addr(const char *p);
+	// 用于将点分十进制IP转换为IPv4的32位无符号整型IP
+	// 将无符号整型IP从主机端序转换为网络端字节序
+	// 最终返回网络端字节序的32位无符号整数IP
 	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	// 端口
+	// htons: 将整型变量从主机字节序转变成网络字节序
+	// 即整数在地址空间上存储方式变为高位字节放在内存的低内存地址处
+	// 网络字节序是TCP/IP中规定好的一种数据表示格式 它与具体的CPU类型
+	// 操作系统无关 从而可以保证数据在不同主机之间传输时能够被正确解释
+	// 网络字节序采用 big-endian 模式
 	serv_addr.sin_port = htons(1234);
+	// 成功 返回 0
+	// 失败 返回 -1
+	// int bind(int sockfd, const struct sockaddr * addr, socklen_t addrlen);
+	// sockfd: 套接字文件描述符 即socket函数创建的套接字文件
+	// addrlen: 第二个参数所指向的结构体变量的大小
+	// addr: 指向一个struct sockaddr类型的结构体变量 此结构体成员变量用于设置
+	// 	 要绑定的ip和端口
+	// 	 sockaddr:{
+	// 	 	// 指定AF_** 表示使用什么协议族的IP格式
+	//		sa_family_t sa_family;
+	//		// 字符数组 用于存放ip和端口
+	//		char sa_data[14]; 
+	// 	 }
 	bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
 
 	// 进入监听状态,等待用户发起请求
+	// int listen(int sock, int backlog);
+	// sock: 需要进入监听状态的套接字
+	// backlog: 请求队列的最大长度
+	// 
+	// 通过listen函数让套接字进入被动监听状态
+	// 所谓被动监听状态，是指当没有客户端请求时
+	// 套接字处于睡眠状态 只有接收到客户端请求时
+	// 套接字才会被唤醒来响应请求
+	//
+	// 请求队列:
+	// 	    当套接字正在处理客户端请求时，如果有新的请求进来
+	// 	    套接字是没有办法处理的 只能把它放进缓冲区 待当前
+	// 	    请求处理完毕之后 再从缓冲区读取出来处理 如果不断
+	// 	    有新的请求进来 它们就按照先后顺序在缓冲区中排队
+	// 	    直到缓冲区满 这个缓冲区就叫做请求队列。
+	// 
+	// 缓冲区长度backlog: 可以通过listen函数的backlog指定 但究竟
+	// 		      为多少并没有什么标准 并发量小的话可以是
+	// 		      10或者20 如果将backlog设置为SOMAXCONN
+	// 		      就由系统决定请求队列的长度 这是backlog
+	// 		      可能是几百或者更多 当请求队列满时 服务器
+	// 		      就不再就收新的请求 对于Linux 客户端会收到
+	// 		      ECONNREFUSED错误 对于Windows 客户端会收到
+	// 		      WSAECONNREFUSED错误
+	//
+	// 注意: listen函数只是让套接字进入监听状态 并没有真正接受客户端请求
+	// 	 listen函数后面的代码会继续执行 直到遇到accept函数 accept()
+	// 	 会阻塞程序执行 直到有新的请求到来。
 	listen(serv_sock, 20);
 
 	// 接受客户端请求
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_size = sizeof(clnt_addr);
+	// int accept(int sock, struct sockaddr *addr, socklen_t *addrlen);
+	// 当套接字处于监听状态时，可以通过accept函数来接收客户端请求
+	// sock: 服务端套接字
+	// addr: IP地址和端口信息
+	// addrlen: addr长度
+	//
+	// accept函数返回一个新的套接字来和客户端通信 addr保存了客户端的IP地址和端口号信息
+	// 而sock是服务端的套接字 后面和客户端通信时，要使用新的套接字而不是原来服务器端的
+	// 套接字
 	int clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
 
 	// 向客户端发送数据
@@ -43,7 +108,9 @@ int main(){
 	write(clnt_sock, str, sizeof(str));
 
 	// 关闭套接字
+	// 关闭当前请求
 	close(clnt_sock);
+	// 关闭服务端套接字
 	close(serv_sock);
 
 	return 0;
